b66bcfaa4ba86ec49ed75abaed8aba5296f95a0a
Moheed-Dar <moheeddar324@gmail.com>
2026-08-22 11:30:18 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactCompiler: true,
+  
+//   images: {
+//     qualities: [75, 90],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  
+  serverExternalPackages: ['sharp'],  
+
   images: {
     qualities: [75, 90],
     remotePatterns: [
